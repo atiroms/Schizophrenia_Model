6c04f0dcf0001a414676b339c987ee7f2efbbcf6
Unknown <atirom.umusus@gmail.com>
2020-02-18 00:27:04 +0900

    param

--- Model_03/Main.py
+++ Model_03/Main.py
@@ -30,7 +30,9 @@ param_batch=[
     #{'name':'learning_rate', 'n':5, 'type':'parametric', 'method':'random', 'min':0.0001, 'max':0.001},
     #{'name':'optimizer', 'n':2, 'type':'list','list':['RMSProp','Adam']}
     #{'name':'gamma','n':3,'type':'parametric','method':'grid','min':0.7,'max':0.9}
-    {'name': 'n_cells_lstm', 'n':20, 'type':'parametric','method':'grid','min':5,'max':100},
+    {'name': 'n_cells_lstm', 'n':20, 'type':'parametric','method':'grid','min':5,'max':100}
+    #{'name': 'learning_rate', 'n':50, 'type':'parametric','method':'grid','min':0.0002,'max':0.0100},
+
 ]
 
 
