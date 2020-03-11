ef6f9d1b7bf5fbeac2c1815b1ea99844e7ef21ca
atiroms <atirom.umusus@gmail.com>
2020-03-11 15:41:28 +0900

    changed TF variables other than Newtork

--- Model_03/Network.py
+++ Model_03/Network.py
@@ -36,11 +36,11 @@ def normalized_columns_initializer(std=1.0):
 class LSTM_RNN_Network():
     def __init__(self,param,n_actions,scope,trainer):
         self.param=param
-        with tf.variable_scope(scope):
+        with tf.compat.v1.variable_scope(scope):
             #Input and visual encoding layers
-            self.prev_rewards = tf.placeholder(shape=[None,1],dtype=tf.float32)
-            self.prev_actions = tf.placeholder(shape=[None],dtype=tf.int32)
-            self.timestep = tf.placeholder(shape=[None,1],dtype=tf.float32)
+            self.prev_rewards = tf.compat.v1.placeholder(shape=[None,1],dtype=tf.float32)
+            self.prev_actions = tf.compat.v1.placeholder(shape=[None],dtype=tf.int32)
+            self.timestep = tf.compat.v1.placeholder(shape=[None,1],dtype=tf.float32)
             self.prev_actions_onehot = tf.one_hot(self.prev_actions,n_actions,dtype=tf.float32)
             
             # Input to LSTM-RNN. timestep is fed
@@ -55,8 +55,8 @@ class LSTM_RNN_Network():
             self.state_init = [c_init, h_init]
 
             # Placeholder of lstm cell states input
-            c_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.c])
-            h_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.h])
+            c_in = tf.compat.v1.placeholder(tf.float32, [1, lstm_cell.state_size.c])
+            h_in = tf.compat.v1.placeholder(tf.float32, [1, lstm_cell.state_size.h])
             self.state_in = (c_in, h_in)
 
             rnn_in = tf.expand_dims(hidden, [0])
@@ -70,7 +70,7 @@ class LSTM_RNN_Network():
             self.state_out = (lstm_c[:1, :], lstm_h[:1, :])
             rnn_out = tf.reshape(lstm_outputs, [-1, int(self.param.n_cells_lstm)])
             
-            self.actions = tf.placeholder(shape=[None],dtype=tf.int32)
+            self.actions = tf.compat.v1.placeholder(shape=[None],dtype=tf.int32)
             self.actions_onehot = tf.one_hot(self.actions,n_actions,dtype=tf.float32)
                         
             #Output layers for policy and value estimations
@@ -85,29 +85,29 @@ class LSTM_RNN_Network():
             
             # Only the agent network need ops for loss functions and gradient updating.
             if scope != 'master':
-                self.value_target = tf.placeholder(shape=[None],dtype=tf.float32)
-                self.advantages = tf.placeholder(shape=[None],dtype=tf.float32)
+                self.value_target = tf.compat.v1.placeholder(shape=[None],dtype=tf.float32)
+                self.advantages = tf.compat.v1.placeholder(shape=[None],dtype=tf.float32)
                 self.responsible_outputs = tf.reduce_sum(self.policy * self.actions_onehot, [1])
 
                 #Loss functions
                 #self.value_loss = 0.5 * tf.reduce_sum(tf.square(self.target_v - tf.reshape(self.value,[-1])))
-                #self.entropy = - tf.reduce_sum(self.policy * tf.log(self.policy + 1e-7))
-                #self.policy_loss = -tf.reduce_sum(tf.log(self.responsible_outputs + 1e-7)*self.advantages)
+                #self.entropy = - tf.reduce_sum(self.policy * tf.math.log(self.policy + 1e-7))
+                #self.policy_loss = -tf.reduce_sum(tf.math.log(self.responsible_outputs + 1e-7)*self.advantages)
                 #self.loss = 0.5 *self.value_loss + self.policy_loss - self.entropy * 0.05
-                self.loss_policy = - tf.reduce_sum(tf.log(self.responsible_outputs + 1e-10)*self.advantages) # advantage as a constant
+                self.loss_policy = - tf.reduce_sum(tf.math.log(self.responsible_outputs + 1e-10)*self.advantages) # advantage as a constant
                 # advantage as a variable. this expression is equivalent to Wang 2018 method
                 self.loss_value = 0.5 * tf.reduce_sum(tf.square(self.value_target - tf.reshape(self.value,[-1])))
-                self.loss_entropy = - tf.reduce_sum(self.policy * tf.log(self.policy + 1e-10))
+                self.loss_entropy = - tf.reduce_sum(self.policy * tf.math.log(self.policy + 1e-10))
                 self.loss_total = self.loss_policy + self.param.cost_statevalue_estimate * self.loss_value - self.param.cost_entropy * self.loss_entropy
 
                 #Get gradients from local network using local losses
-                vars_local = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
+                vars_local = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.TRAINABLE_VARIABLES, scope)
                 self.gradients = tf.gradients(self.loss_total,vars_local)
                 # return square root of the sum of squares of l2 norms of the input tensors
-                self.norms_var = tf.global_norm(vars_local)
+                self.norms_var = tf.linalg.global_norm(vars_local)
                 # return a list of tensors clipped using global norms
                 grads,self.norms_grad = tf.clip_by_global_norm(self.gradients,50.0)
                 
                 # Apply local gradients to master network
-                vars_master = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'master')
+                vars_master = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.TRAINABLE_VARIABLES, 'master')
                 self.apply_grads = trainer.apply_gradients(zip(grads,vars_master))
